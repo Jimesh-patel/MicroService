e76333f206519fad4c66305da0199473b638a122
Jimesh-patel <vadodariyajimesh1811@gmaiil.com>
2025-07-13 13:47:03 +0530

    Test : Working correctly...

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    // This ensures that on refresh, all routes serve index.html
+    historyApiFallback: true,
+  }
 })
